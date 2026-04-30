aa02ca4ec8f2ae8eda0d71fba5c7343e93ff0e54
Augusto Terrera <augustoterrera@gmail.com>
2026-04-30 14:09:43 -0300

    fix: actualizar URL del sitio en la configuración de Astro

--- gesper/astro.config.mjs
+++ gesper/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL ?? 'https://gesper.com',
+  site: process.env.PUBLIC_SITE_URL ?? 'https://gesper.noanet.com',
   integrations: [tailwind()],
 });
